4daab99d0e7fc8e8bb0f78e6527c4a3cd5165507
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2024-03-11 14:03:22 +0100

    vari

--- lezione1/vari.php
+++ lezione1/vari.php
@@ -4,6 +4,15 @@ $num = 7.2;
 $name = "6";
 $nomeCognome = "Mario Rossi";
 
+$mioIntero = -7;
+var_dump($mioIntero);
+
+$mioFloat = 5.673;
+var_dump($mioFloat);
+echo "<hr>";
+echo $mioFloat;
+echo "<hr>";
+
 echo $name + $num;
 
 echo "<hr>";
@@ -22,6 +31,9 @@ echo "Number is $num<br>";
  */
 function somma(int $a, float $b)
 {
+    var_dump($a);
+    var_dump($b);
     return $a + $b;
+
 }
-echo somma(7, "5");
+echo somma(-2, "5");
