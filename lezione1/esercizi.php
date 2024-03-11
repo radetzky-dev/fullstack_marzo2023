4f05d4f43dcb005d5062187fa048e80af948b9fd
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2024-03-11 14:16:25 +0100

    esercizi

---
+++ lezione1/esercizi.php
@@ -0,0 +1,20 @@
+<?php
+
+$myVar = "musa formazione";
+
+/*
+https://www.php.net/manual/it/language.types.string.php
+https://www.php.net/manual/it/ref.strings.php
+
+
+a) transform a string all uppercase letters.
+b) transform a string all lowercase letters.
+c) make a string's first character uppercase.
+d) make a string's first character of all the words uppercase.
+*/
+
+$myVar = "Il cielo oggi è azzurro";
+/*
+Se $myvar contine la parola "oggi"
+*/
+
