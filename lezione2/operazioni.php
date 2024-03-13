074ca99d3a93fcdadc0a9b01dcb05e4da22ab857
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2024-03-13 12:28:24 +0100

    operatori

---
+++ lezione2/operazioni.php
@@ -0,0 +1,47 @@
+<?php
+
+$x = 6;
+
+echo $x + 5;
+echo "<hr>";
+
+echo $x - 2;
+
+echo "<hr>";
+echo $x % 2;
+
+echo "<hr>";
+echo "<br>divisione ";
+echo $x / 3;
+echo "<br>resto ";
+echo $x % 3;
+
+echo "<hr>";
+echo "<br>divisione ";
+echo $x / 5;
+echo "<br>resto ";
+echo $x % 5;
+
+//ASSEGNAZIONE
+
+$a = ($b = 4) + 5; // $a is equal to 9 now, and $b has been set to 4.
+
+echo "<hr>";
+$a = 3;
+$a += 5;
+echo $a;
+
+
+echo "<hr>";
+$a = 3;
+$a = $a + 5;
+echo $a;
+
+echo "<hr>";
+$b = "Hello ";
+$b .= "There!";
+
+//$b = $b . "There!";
+echo $b;
+
+
