f6aecadfc6cfe970b1cab12f7ebd589467774a3c
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2024-03-13 10:04:38 +0100

    array

---
+++ lezione2/array.php
@@ -0,0 +1,22 @@
+<?php
+
+$vari = array('fiat', 'volvo', 'ferrari', 7, 7.56);
+var_dump($vari);
+echo '<hr>';
+
+$cars = array('fiat', 'volvo', 'ferrari');
+var_dump($cars);
+
+echo '<hr>';
+
+print_r($cars);
+
+echo '<hr>';
+
+echo $cars[1];
+
+echo '<hr>';
+
+echo $cars[2];
+
+
