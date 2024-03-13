c0267446bb4d1c202c9ab58e5726e47894a61d95
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2024-03-13 10:27:26 +0100

    tipi di indici

--- lezione2/array.php
+++ lezione2/array.php
@@ -1,5 +1,6 @@
 <?php
 
+//ARRAY NUMERICI
 $vari = array('fiat', 'volvo', 'ferrari', 7, 7.56);
 var_dump($vari);
 echo '<hr>';
@@ -26,5 +27,29 @@ echo '<hr>';
 
 echo $cars[3];
 
+echo '<hr>';
+//ARRAY ASSOCIATIVI
 
+$ages = array("Mario" => 11, "Maria" => 31, "Silvia" => 18);
+var_dump($ages);
 
+echo '<hr>';
+echo "Mario ha " . $ages['Mario'] . " anni<br>";
+echo "Silvia ha " . $ages['Silvia'] . " anni<br>";
+
+//ARRAY Multidimensionali
+
+$families = array(
+    "Griffin" => array('Peter', 'Lois', 'Megan'),
+    "Quagmaire" => array('Glenn', 'Maria'),
+    "Rossi" => array('Piero', 'Silvia', 'Antonio'),
+    "Verdi" => array("Mario" => 11, "Maria" => 'trenta'),
+    "gialli" => array(81 => 11, 3 => 'trenta'),
+);
+
+var_dump($families['Rossi'][2]);
+echo '<hr>';
+echo "Mario dei Verdi ha " . $families['Verdi']['Maria'] . " anni";
+echo "<hr>";
+var_dump($families['gialli'][81]);
+var_dump($families['gialli'][3]);
