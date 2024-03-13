79a996e89da477f8caf6d91c84a3b031af28999d
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2024-03-13 11:26:57 +0100

    base musa

--- lezione2/esercizio_array.php
+++ lezione2/esercizio_array.php
@@ -1,13 +1,45 @@
 <?php
+$siteTitle = "General Store Musa";
+//Assegna prodotti e prezzi all'array products
+$products = array(
+    "pc" => array(600.20, "torta.png"),
+    "mouse" => array(8.50, "torta.png")
+);
+?>
 
-$arr = array("uno", "martello", "trave di legno");
-foreach ($arr as $indice => $valore) {
-    echo "{$indice} => {$valore} <br>";
-}
-echo "<br>";
+<!DOCTYPE html>
+<html lang="it">
 
-$arr = array("uno" => "ciao", "martello" => "3kg", "trave di legno" => "abete");
-foreach ($arr as $indice => $valore) {
-    echo "{$indice} => {$valore} <br>";
-}
-echo "<br>";
+<head>
+    <meta charset="UTF-8">
+    <title>
+        <?php echo $siteTitle; ?>
+    </title>
+</head>
+
+<body>
+    <h2><strong>
+            <?php echo $siteTitle; ?>
+        </strong></h2>
+
+    <h3>I nostri prodotti</h3>
+
+    <table border="1">
+        <thead>
+            <tr>
+                <td>Oggetto</td>
+                <td>Prezzo</td>
+                <td>Immagine</td>
+            </tr>
+        </thead>
+        <?php
+        //Stampa prodotti e relativi prezzi in un ciclo foreach
+        foreach ($products as $index => $value) {
+            echo "<tr><td>" . $index . "</td><td>" . $value[0] . "</td><td><img src='assets/" . $value[1] . "'></td></tr>";
+
+        }
+        ?>
+    </table>
+</body>
+
+</html>
