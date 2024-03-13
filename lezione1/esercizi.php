2c2e27a65609c8a15658a3caf9063add89055baa
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2024-03-13 09:51:17 +0100

    esercizio

--- lezione1/esercizi.php
+++ lezione1/esercizi.php
@@ -12,9 +12,42 @@ b) transform a string all lowercase letters.
 c) make a string's first character uppercase.
 d) make a string's first character of all the words uppercase.
 */
+$myVarUppercase = strtoupper($myVar);
+echo $myVarUppercase; // Output: MUSA FORMAZIONE
+echo "<br>";
+$myVarLowercase = strtolower($myVar);
+echo $myVarLowercase; // Output: musa formazione
+echo "<br>";
+$myVarFirstUppercase = ucfirst($myVar);
+echo $myVarFirstUppercase; // Output: Musa formazione
+echo "<br>";
+$myVarWordsUppercase = ucwords($myVar);
+echo $myVarWordsUppercase; // Output: Musa Formazione
+echo "<hr>";
 
-$myVar = "Il cielo oggi è azzurro";
+$myString = "Il cielo oggi è azzurro";
 /*
 Se $myvar contine la parola "oggi"
 */
+$search = "oGgI";
 
+$search = strtolower($search); //normalization
+
+echo "<h3>$myString</h3>";
+echo "<em>controllo con la funzione str_contains()</em> <br>";
+// using the function str_contains(). case-sensitive!
+if (str_contains($myString, $search)) {
+    echo "La stringa CONTIENE la parola <strong>'$search'</strong>.<br><hr>";
+} else {
+    echo "La stringa NON contiene la parola <strong>'$search'</strong>.<br><hr>";
+}
+
+// using strripos(). Case unsensitive function
+$contains;
+echo "<em>controllo con la funzione strripos()</em><br>";
+if (strripos($myString, $search[0]) !== false) {
+    $contains = "";
+} else {
+    $contains = "non";
+}
+echo "La stringa $contains contiene la parola <strong>'$search'</strong>";
