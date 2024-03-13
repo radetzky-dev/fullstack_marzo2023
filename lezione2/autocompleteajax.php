30c98872efba480fe8a93bb0923c1c047b88e0f9
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2024-03-13 14:13:16 +0100

    esercizio

---
+++ lezione2/autocompleteajax.php
@@ -0,0 +1,34 @@
+<html>
+
+<head>
+    <script>
+        function showHint(str) {
+            if (str.length == 0) {
+                document.getElementById("txtHint").innerHTML = "";
+                return;
+            } else {
+                var xmlhttp = new XMLHttpRequest();
+                xmlhttp.onreadystatechange = function () {
+                    if (this.readyState == 4 && this.status == 200) {
+                        document.getElementById("txtHint").innerHTML = this.responseText;
+                    }
+                };
+                xmlhttp.open("GET", "gethint.php?q=" + str, true);
+                xmlhttp.send();
+            }
+        }
+    </script>
+</head>
+
+<body>
+    <p><b>Scrivi il nome che cerchi:</b></p>
+    <form action="">
+        <label for="fname">Nome:</label>
+        <input type="text" id="fname" name="fname" onkeyup="showHint(this.value)">
+        <input type="text" id="ftel" name="ftel" onkeyup="showTel(this.value)">
+    </form>
+    <p>Suggerimento nome: <span id="txtHint"></span></p>
+    <p>Suggerimento telefono: <span id="txtTel"></span></p>
+</body>
+
+</html>
