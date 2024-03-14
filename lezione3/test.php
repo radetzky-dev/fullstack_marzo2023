898e4b4813c64884ded6036caca68eefbf1d2bdd
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2024-03-14 10:12:40 +0100

    ternario

---
+++ lezione3/test.php
@@ -0,0 +1,27 @@
+<?php
+$name = "Paolo";
+echo "Prova<br>";
+print "print does not require parentheses.";
+
+$foo = "example";
+print "foo is $foo<br>";
+echo "questo è un $foo<br>";
+
+$myNum = 29;
+
+echo ($myNum <= 30) ? "Si" : "No";
+
+$myName = "Paolddsso";
+
+echo ($myName == "Paolo") ? "Ciao" : "Non ti conosco";
+
+
+
+?>
+<p>QUi</p>
+
+<p>Ciao,
+    <b>
+        <?= $name; ?>
+    </b> stai bene?
+</p>
