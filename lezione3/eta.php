638ee7c19e20194a5d68c9b4f593f29644bd8dc7
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2024-03-14 10:47:25 +0100

    es

--- lezione3/eta.php
+++ lezione3/eta.php
@@ -2,3 +2,5 @@
 
 //una persona che ha un età e voi dovete mostrare un messaggio se può bere alcolici o meno in Italia 18
 //e in USA 21
+
+//Secondo dato un numero dire se è positivo, negativo o se è zero
