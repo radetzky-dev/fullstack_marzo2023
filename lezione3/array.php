0d41191d11a1b96e8160de5b54bd8430acac9dd5
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2024-03-14 13:02:30 +0100

    array

---
+++ lezione3/array.php
@@ -0,0 +1,26 @@
+<?php
+
+$a = array("a", "b");
+
+$b = ["a", "b"];
+
+$b[] = "c";
+
+var_dump($a);
+echo "<hr>";
+var_dump($b);
+
+$frutti = ["frutto0" => "banana", "frutto1" => "mela"];
+
+echo "Mi mangio una " . $frutti["frutto1"];
+echo "<hr>";
+$frutti["frutto1"] = "lampone";
+$frutti["fruttoX"] = "pera";
+$frutti[] = "anguria";
+$frutti["fruttoY"] = "kiwi";
+$frutti[] = "melone";
+echo "Mi mangio una " . $frutti["frutto1"];
+echo "<hr>";
+echo "<pre>";
+print_r($frutti);
+echo "</pre>";
