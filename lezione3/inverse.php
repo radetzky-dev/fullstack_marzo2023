9fe814068e877ffe923ecddae783bf3817f7dcdd
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2024-03-15 10:33:51 +0100

    soluzione

--- lezione3/inverse.php
+++ lezione3/inverse.php
@@ -1,9 +1,23 @@
-<?
+<?php
 
-/*
-Write a function to reverse a string
-paolo -> oloap
+/**
+ * reverse_string
+ *
+ * @param  mixed $str1
+ * @return string
+ */
+function reverse_string(string $str1): string|null
+{
+    $n = strlen($str1);
+    // echo "$n $str1<br>";
+    if ($n == 1) {
+        return $str1;
+    } else {
+        $n--;
+        return reverse_string(substr($str1, 1, $n)) . substr($str1, 0, 1);
+    }
+}
+echo '<br>';
+echo reverse_string('paolo rossi verdi') . "<br>";
 
-reverseString
-
-*/
+echo strrev("paolo rossi");
