c2d1bb7250dd57d1897bef0625dfb96abc647e05
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2024-03-15 10:35:35 +0100

    reverse

--- lezione3/inverse.php
+++ lezione3/inverse.php
@@ -1,12 +1,12 @@
 <?php
 
 /**
- * reverse_string
+ * reverseString
  *
  * @param  mixed $str1
  * @return string
  */
-function reverse_string(string $str1): string|null
+function reverseString(string $str1): string|null
 {
     $n = strlen($str1);
     // echo "$n $str1<br>";
@@ -14,10 +14,10 @@ function reverse_string(string $str1): string|null
         return $str1;
     } else {
         $n--;
-        return reverse_string(substr($str1, 1, $n)) . substr($str1, 0, 1);
+        return reverseString(substr($str1, 1, $n)) . substr($str1, 0, 1);
     }
 }
 echo '<br>';
-echo reverse_string('paolo rossi verdi') . "<br>";
+echo reverseString('paolo rossi verdi') . "<br>";
 
 echo strrev("paolo rossi");
