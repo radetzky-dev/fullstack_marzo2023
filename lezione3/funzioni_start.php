691df89019fdea3d607fa5f6f958b33a307b2253
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2024-03-15 10:14:39 +0100

    params

--- lezione3/funzioni_start.php
+++ lezione3/funzioni_start.php
@@ -16,6 +16,23 @@ function writeName(string $name, string $surname): string
 function scriviNome($nome)
 {
     echo "ciao $nome";
+}
+
+
+/**
+ * scrivEta
+ *
+ * @param  mixed $name
+ * @param  mixed $age
+ * @return void
+ */
+function scrivEta(string $name, int $age = 0): void
+{
+    if ($age > 0) {
+        echo "$name ha $age anni<br>";
+    } else {
+        echo "Ciao $name<br>";
+    }
 
 }
 
@@ -47,3 +64,9 @@ echo addRef($myNumber) . '<br>';
 echo addRef($myNumber) . '<br>';
 echo add($myNumber) . '<br>';
 echo "MyNum $myNumber<br>";
+
+echo "<hr>";
+scrivEta("mario");
+scrivEta("piero", 32);
+scrivEta("silvia", 8);
+
