d38e95b1b985a7f17a127ee51e289035f0d00e3e
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2024-03-15 12:17:43 +0100

    versione

---
+++ lezione4/varie.php
@@ -0,0 +1,10 @@
+<?php
+
+
+if (function_exists('json_validate')) {
+    echo "funzione  are available.<br />\n";
+    var_dump(json_validate('{ "test": { "foo": "bar" } }'));
+} else {
+    echo "funzione are NOT available.<br />\n";
+    echo "uso un'altra funzione...";
+}
