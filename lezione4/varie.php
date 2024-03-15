6e132b2f2ec1e8b323ca8bf44bdb4908b738d071
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2024-03-15 13:30:29 +0100

    tipi

--- lezione4/varie.php
+++ lezione4/varie.php
@@ -43,3 +43,39 @@ var_dump(isset($a, $b)); // FALSE
 
 $foo = NULL;
 var_dump(isset($foo));   // FALSE
+
+
+echo "<hr>";
+$varx = 0;
+
+// Evaluates to true because $var is empty
+if (empty($varx)) {
+    echo '$var is either 0, empty, or not set at all<br>';
+}
+
+// Evaluates as true because $var is set
+if (isset($varx)) {
+    echo '$var is set even though it is empty<br>';
+}
+
+echo "<hr>";
+$array = ['a', 7, "b"];
+$data = array(1, 1., NULL, new stdClass, 'foo', $array);
+
+foreach ($data as $value) {
+    echo gettype($value), "<br>";
+}
+echo "<hr>";
+
+$foo = "5bar"; // string
+$foo1 = 20.5; // string
+$bar = true;   // boolean
+
+settype($foo, "integer"); // $foo is now 5   (integer)
+echo $foo . "<br>";
+settype($bar, "string");  // $bar is now "1" (string)
+echo $bar . "<br>";
+settype($foo1, "string");
+echo $foo1 . " " . gettype($foo1) . "<br>";
+settype($foo1, "integer");
+echo $foo1 . " " . gettype($foo1) . "<br>";
