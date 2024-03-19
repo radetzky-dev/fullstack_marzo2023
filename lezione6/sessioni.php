142b60fe9eb7ddf98d163aa67e9ca2d91d1a0a33
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2024-03-19 12:49:05 +0100

    sessioni

---
+++ lezione6/sessioni.php
@@ -0,0 +1,18 @@
+<?php
+
+session_start();
+if (!isset ($_SESSION['count'])) {
+    $_SESSION['count'] = 0;
+} else {
+    $_SESSION['count']++;
+}
+
+$_SESSION['carrello'] = ["t-shirt", 30, "XL"];
+$_SESSION['nome'] = "Mario";
+
+
+echo $_SESSION['nome'] . " hai visitato questa pagina " . $_SESSION['count'] . " volte";
+echo "<br>Cosa c'è nel carrello?  ";
+var_dump($_SESSION['carrello']);
+
+//unset($_SESSION['count']);
