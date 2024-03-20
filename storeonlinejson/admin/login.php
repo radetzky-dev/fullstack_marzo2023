6289b51f91a9ecd63e3331abe55c52779b6463f9
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2024-03-20 12:01:49 +0100

    login

--- storeonlinejson/admin/login.php
+++ storeonlinejson/admin/login.php
@@ -3,6 +3,8 @@ require_once "../inc/functions.php";
 require_once "../inc/header.php";
 session_start();
 ?>
+
+
 <div>
     <h3>Admin login</h3>
     <?php
@@ -15,22 +17,25 @@ session_start();
         unset($_SESSION["errorMessage"]);
     }
     ?>
-    <form action="login-action.php" method="post" id="frmLogin">
+    <section class="w-100 p-4 d-flex justify-content-center pb-4">
 
-        <div class="form-outline mb-4">
-            <label class="text-left" for="username">Username <span id="user_info"
-                    class="validation-message"></span></label>
-            <input name="user_name" id="user_name" type="text" class="full-width" required>
-        </div>
-        <div class="form-outline mb-4">
-            <label class="text-left" for="password">Password <span id="password_info"
-                    class="validation-message"></span></label> <input name="password" id="password" type="password"
-                class="full-width" required>
-        </div>
-        <div class="form-outline mb-4">
-            <input type="submit" name="login" value="Login" class="btn btn-primary btn-block mb-4""></span>
-        </div>
-    </form>
+        <form action="login-action.php" method="post" id="frmLogin" style="width: 30rem;">
+
+            <div class="form-outline mb-4">
+                <label class="text-left" for="username">Username <span id="user_info"
+                        class="validation-message"></span></label>
+                <input name="user_name" id="user_name" type="text" class="full-width" required>
+            </div>
+            <div class="form-outline mb-4">
+                <label class="text-left" for="password">Password <span id="password_info"
+                        class="validation-message"></span></label> <input name="password" id="password" type="password"
+                    class="full-width" required>
+            </div>
+            <div class="form-outline mb-4">
+                <input type="submit" name="login" value="Login" class="btn btn-primary btn-block mb-4""></span>
+</div>
+</form>
+</section>
 </div>
 
 <?php
