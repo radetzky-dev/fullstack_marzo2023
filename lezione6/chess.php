20b7fca4d5d2155ee8c9ade90e24c29ccbca299d
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2024-03-20 09:43:33 +0100

    soluzione chess

---
+++ lezione6/chess.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Scacchiera</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+    <style>
+        .black {
+            background-color: #000000;
+        }
+
+        .white {
+            background-color: #FFFFFF;
+        }
+    </style>
+</head>
+
+<body>
+    <h3>Schacchiera</h3>
+    <table width="270px" cellspacing="0px" cellpadding="0px" border="1px">
+        <?php
+        for ($row = 1; $row <= 8; $row++) {
+            echo "<tr>";
+            for ($col = 1; $col <= 8; $col++) {
+                $total = $row + $col;
+                if ($total % 2 == 0) {
+                    echo "<td height=30px width=30px class='black'></td>";
+                } else {
+                    echo "<td height=30px width=30px class='white'></td>";
+                }
+            }
+            echo "</tr>";
+        }
+        ?>
+    </table>
+</body>
+
+</html>
