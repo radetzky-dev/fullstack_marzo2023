d5a81e12ada4d7d5dcde0a8a591ccbdd01cc7166
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2024-03-20 12:31:36 +0100

    home

--- storeonlinejson/index.php
+++ storeonlinejson/index.php
@@ -3,65 +3,19 @@ session_start();
 require_once "inc/functions.php";
 require_once "inc/header.php";
 
-?>
-
-<a href='#' class='btn btn-primary' onclick='return test();'>test</a>
-
-<?php
 if (isset ($_SESSION["role"]) && $_SESSION["role"] == "admin") { ?>
     <h3>Menù per la gestione AMMINISTRATORI</h3>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti placeat quo, repellendus eveniet facere
-        tempora asperiores suscipit vitae aspernatur quisquam? Hic perspiciatis aperiam nulla magni iste labore
-        provident voluptates nostrum?</p>
+    <p>SCRIEVRE O METTERE LINK PER AMMINISTRATORE</p>
 <?php } ?>
 
-<table class="table table-bordered" id="tabella">
-    <thead thead class="table-dark">
-        <tr>
-            <th>ID</th>
-            <th>Qta</th>
-            <th>Marca</th>
-            <th>Descrizione</th>
-            <th>Prezzo</th>
-            <th>Categoria</th>
-            <th>Buy</th>
-            <th>Delete</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php
 
-        /*  if (!isset ($strumentiMusicali)) {
-              $myFile = readContentFile($pathData);
-              $strumentiMusicali = json_decode($myFile, true);
-          }
-
-          $categorie = [];
-
-          foreach ($strumentiMusicali['items'] as $key => $strumenti) {
-              foreach ($strumenti as $strumento) {
-
-                  $delete = "<a href='" . $_SERVER['PHP_SELF'] . "?op=delete&id=" . $strumento['id'] . "' class='btn btn-primary' onclick='return confirmDelete();'>$deleteIcon</a>";
-
-                  $buy = "<a href='" . $_SERVER['PHP_SELF'] . "?op=buy&id=" . $strumento['id'] . "&price=" . $strumento['price'] . "&marca=" . $strumento['marca'] . "' class='btn btn-primary'>$cartIcon</a>";
-
-                  echo '<tr><td>' . $strumento['id'] . '</td><td>' . $strumento['number'] . '</td><td>' . strtoupper($strumento['marca']) . ' </td><td>' . $strumento['name'] . '</td><td> ' . $strumento['price'] . '</td><td><strong>' . strtoupper($key) . '</strong></td><td>' . $buy . '</td><td>' . $delete . '</td></tr>';
-                  $categorie[] = $key;
-
-              }
-
-          }
-          */
-        ?>
-    </tbody>
-</table>
 
 
 <div class="row">
     <div class="col-sm-6">
         <div class="card">
             <div class="card-body">
-                QUI TABELLA
+                QUI TABELLA DOVE MOSTRIAMO 3 PRODOTTI
             </div>
         </div>
     </div>
